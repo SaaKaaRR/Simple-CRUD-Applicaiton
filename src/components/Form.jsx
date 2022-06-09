8cb6b840e5132564ec59f0de23ffb398594d8130
Sakar Poudel <ultimatesakar@gmail.com>
2022-06-10 00:15:15 +0545

    Tried to post data from axios using form data to send the data of form but an error occured. Stull have some issues to fux in that

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,41 +1,76 @@
-import React, { useState } from 'react'
-import axios from "axios"
-import Card from './Card'
+import React, { useState } from "react";
+import axios from "axios";
+import Card from "./Card";
 
 function Form() {
-  const [data,setData]= useState([])
-  const getAllPosts = ()=>{
-    axios.get("http://localhost:5000/posts").then((data)=>{
-    
-     setData(data.data.PostMessages)
-    })
-    .catch((err)=>{
-      console.log("Error is",err)
-    })
+  const BackendURL = "http://localhost:5000/posts";
+  const [data, setData] = useState([]);
+  const getAllPosts = () => {
+    axios
+      .get("http://localhost:5000/posts")
+      .then((data) => {
+        setData(data.data.PostMessages);
+      })
+      .catch((err) => {
+        console.log("Error is", err);
+      });
+  };
+  function SendPost() {
+    const postData = new FormData();
+    postData.append("name", "Sakar");
+    console.log({postData});
+    axios
+      .post(BackendURL, {
+        data: postData,
+      })
+      .then(() => {
+        console.log("post done");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
-  function mapDeletedData(id){
-    const newData=data.filter((post)=>{
-      return post._id!=id
-    })
-    setData(newData)
+  function mapDeletedData(id) {
+    const newData = data.filter((post) => {
+      return post._id !== id;
+    });
+    setData(newData);
   }
   return (
-    <div class ="container ml-3">
-        <form action="http://localhost:5000/posts/" method= "post">
-            <input type="text" name="creator" placeholder='Your Name'/>
-            <input type="text" name="title" placeholder='Enter your title'/>
-            <input type="text" name="message" placeholder='Type your post here'/>
-            
-            <button type="submit" class="btn btn-success" name="PostButton">Post</button>
-        </form>
+    <div class="container ml-3">
+      <form id="postForm">
+        <input type="text" name="creator" placeholder="Your Name" />
+        <input type="text" name="title" placeholder="Enter your title" />
+        <input type="text" name="message" placeholder="Type your post here" />
+      </form>
+      <button
+        type="submit"
+        class="btn btn-success"
+        name="PostButton"
+        onClick={SendPost}
+      >
+        Post
+      </button>
 
-       
-       <button class='btn btn-success' name='getPostButton' onClick={()=>getAllPosts()}>Get All Posts</button>
-        {data.map((one)=>{
-          return <Card id={one._id} title={one.title} message={one.message} mapDeletedData={mapDeletedData}/>
-        })}
+      <button
+        class="btn btn-success"
+        name="getPostButton"
+        onClick={() => getAllPosts()}
+      >
+        Get All Posts
+      </button>
+      {data.map((one) => {
+        return (
+          <Card
+            id={one._id}
+            title={one.title}
+            message={one.message}
+            mapDeletedData={mapDeletedData}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
